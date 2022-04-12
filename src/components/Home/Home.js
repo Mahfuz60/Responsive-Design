9ed846e20680cd56e6a54b72a57219c43267edea
mahfuz60 <mahfuzuralam1996@gmail.com>
2022-04-13 00:47:15 +0600

    first commit basic setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div className='Box-1' style={{ color: 'red', fontSize: '40px', textAlign: 'center' }}>
+        Box-1
+      </div>
+    </div>
+  );
+};
+
+export default Home;
